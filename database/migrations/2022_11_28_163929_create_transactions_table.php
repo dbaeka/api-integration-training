033b351d75025a5c62dab025ca4001cc498282f4
Delmwin Baeka <dbaekajnr@gmail.com>
2022-11-28 16:44:00 +0000

    :construction: add transaction model

---
+++ database/migrations/2022_11_28_163929_create_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('internal_reference');
+            $table->string('type', 40);
+            $table->string('status_code', '3')->default(411);
+            $table->string('buyer_name')->nullable();
+            $table->string('buyer_msisdn');
+            $table->string('country', 2);
+            $table->string('currency', 3);
+            $table->string('amount');
+            $table->string('third_party_reference');
+            $table->string('description', 300)->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
